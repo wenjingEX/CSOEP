c4a55ec30ef649997fbf7e4cd89c677b4e5231fa
wenjing <3139488014@qq.com>
2023-07-12 23:46:29 +0800

    第一次搭建环境

---
+++ src/test/java/com/CS/csoep/CsoepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS.csoep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsoepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
