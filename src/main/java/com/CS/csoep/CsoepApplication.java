53b9e31615aa79664872918269c032f07a490dcd
wenjing <3139488014@qq.com>
2023-07-13 00:12:20 +0800

    第一次搭建环境

--- src/main/java/com/CS/csoep/CsoepApplication.java
+++ src/main/java/com/CS/csoep/CsoepApplication.java
@@ -8,7 +8,7 @@ public class CsoepApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CsoepApplication.class, args);
-        System.out.println("项目启动成功.....");
+        System.out.println("项目启动成功......");
     }
 
 }
